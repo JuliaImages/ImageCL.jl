996c09be03fabc7faaf6a1e9ccc3de0d371bdc32
jingpengwu <jingpeng.wu@gmail.com>
2017-02-23 17:39:40 -0500

    make gaussian works with latest OpenCL and Images in Julia 0.5

--- src/ImageCL.jl
+++ src/ImageCL.jl
@@ -1,10 +1,11 @@
 module ImageCL
 
+export load_kernel
 # package code goes here
 
 load_kernel(name) = open(readall, joinpath(dirname(@__FILE__), "..", "kernel", name))
 
 #include("orb.jl")
-include("gaussian_blur.jl")
+# include("gaussian_blur.jl")
 
 end # module
--- src/gaussian_blur.jl
+++ src/gaussian_blur.jl
@@ -1,22 +1,26 @@
-import OpenCL
+using OpenCL
+import  OpenCL: cl
 using Colors, FileIO, Images
+using ImageCL
+using ImageView
 
+image = map(Colors.RGBA{Float32}, load(joinpath(dirname(@__FILE__()), "../assets/cups.jpg")))
+imshow(image)
 
-image = map(RGBA{Float32}, load(joinpath(homedir(), "surface.png"))).data
+device, ctx, queue = OpenCL.cl.create_compute_context()
+const gauss_kernel_src = ImageCL.load_kernel("gaussian_blur.cl")
+gauss_program = cl.Program(ctx, source=gauss_kernel_src) |> cl.build!
+gauss_cl_func = cl.Kernel(gauss_program, "fir_ver_blur")
 
-const cl = OpenCL
-device, ctx, queue = cl.create_compute_context()
-const gauss_kernel_src = load_kernel("gaussian_blur.cl")
-kernel = map(Float32, gaussian2d(2.0, (5,5)))
 # create opencl buffer objects
 # copies to the device initiated when the kernel function is called
+kernel = map(Float32, gaussian2d(2.0, (5,5))).parent
 img_buff = cl.Buffer(RGBA{Float32}, ctx, (:r, :copy), hostbuf=image)
 blurred_buff = cl.Buffer(RGBA{Float32}, ctx, :w, length(image))
 kernel_buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=kernel)
 
 # build the program and construct a kernel object
-gauss_program = cl.build!(cl.Program(ctx, source=gauss_kernel_src))
-gauss_cl_func = cl.Kernel(gauss_program, "fir_ver_blur")
+
 
 # const global float4 *src_buf,
 # const int src_width,
@@ -26,13 +30,11 @@ gauss_cl_func = cl.Kernel(gauss_program, "fir_ver_blur")
 # const int yoff
 
 # call the kernel object with global size set to the size our arrays
-
-gauss_cl_func[queue, size(image)](
-    img_buff, Cint(size(image, 1)),
-    blurred_buff,
-    kernel_buff, Cint(length(kernel)),
-    Cint(0)
-)
+@show gauss_cl_func
+gauss_cl_func[queue, size(image)](  img_buff, Cint(size(image, 1)),
+                                    blurred_buff,
+                                    kernel_buff, Cint(length(kernel)),
+                                    Cint(0));
 
 # perform a blocking read of the result from the device
 result_img = reshape(cl.read(queue, blurred_buff), size(image))
@@ -42,4 +44,6 @@ Base.clamp(x::Float32) = isnan(x) ? U8(0) : U8(clamp(x, 0.0f0, 1.0f0))
 img = map(result_img) do x
     RGBA{U8}(clamp(red(x)), clamp(green(x)), clamp(blue(x)), clamp(alpha(x)))
 end
-save("test.png", img)
+# save("test.png", img)
+using ImageView
+imshow(img)
